a2d7a64a901823a4940188feaa88b887014ae6fe
Georgian Camarasan <georgian.camarasan@evozon.com>
2022-01-02 11:49:10 +0200

    Removed extra space.

--- .config/nvim/lua/user/plugins.lua
+++ .config/nvim/lua/user/plugins.lua
@@ -127,7 +127,7 @@ return packer.startup(function(use)
 
   use {
     'glacambre/firenvim',
-    run = function() vim.fn['firenvim#install'](0) end 
+    run = function() vim.fn['firenvim#install'](0) end
   }
 
   use {
