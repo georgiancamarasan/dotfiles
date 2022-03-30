0a9d87c38d33a125ee12afc2952fd7e609a0814b
Georgian Camarasan <georgian.camarasan@zohomail.eu>
2022-03-30 08:42:15 +0300

    Added display option for WSL2.

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,4 +1,5 @@
-set PATH $PATH:/home/georgian/.cargo/bin
-set PATH $PATH:/home/georgian/.local/bin
+set -x PATH $PATH:/home/georgian/.cargo/bin
+set -x PATH $PATH:/home/georgian/.local/bin
+set -x DISPLAY 192.168.1.121:0
 zoxide init fish | source
 fish_vi_key_bindings
