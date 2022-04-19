0fc68d00a73c07dd31489440e93191ce6c40c199
Georgian Camarasan <georgian.camarasan@zohomail.eu>
2022-04-19 17:12:02 +0300

    Moved fish variables to their own file.

---
+++ .config/fish/conf.d/variables.fish
@@ -0,0 +1,4 @@
+set -x PATH $PATH:/home/georgian/.cargo/bin
+set -x PATH $PATH:/home/georgian/.local/bin
+set -x EDITOR nvim
+set -x DISPLAY 192.168.1.121:0
--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,5 +1,2 @@
-set -x PATH $PATH:/home/georgian/.cargo/bin
-set -x PATH $PATH:/home/georgian/.local/bin
-set -x DISPLAY 192.168.1.121:0
 zoxide init fish | source
 fish_vi_key_bindings
