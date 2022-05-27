8d902a2d16adaa76a71b635c4b5d59bd7dcf9a13
Georgian Camarasan <georgian.camarasan@zohomail.eu>
2022-05-27 23:25:13 +0300

    Installed pueue and added install_cargo_apps function

---
+++ .config/fish/functions/install_cargo_apps.fish
@@ -0,0 +1,20 @@
+function install_cargo_apps
+  curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
+
+  cargo install pueue
+
+  systemctl --user daemons-reload
+  systemctl --user enable --now pueued
+
+  pueue add cargo install bat
+  pueue add cargo install cargo-update
+  pueue add cargo install binstall
+  pueue add cargo install diskonaut
+  pueue add cargo install fd-find
+  pueue add cargo install just
+  pueue add cargo install ripgrep
+  pueue add cargo install rm-improved
+  pueue add cargo install xsv
+  pueue add cargo install zoxide
+
+end
