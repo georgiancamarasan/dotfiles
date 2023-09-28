1d8c48ec69bd725e9f00e4ebf1fb5289de12821b
Georgian Camarasan <georgian.camarasan@zohomail.eu>
2023-09-28 22:14:14 +0300

    Organized the programs in home-manager.

--- home-manager/.config/home-manager/home.nix
+++ home-manager/.config/home-manager/home.nix
@@ -6,7 +6,30 @@
   home.homeDirectory = "/home/georgian";
 
   home.stateVersion = "23.05";
-  programs.home-manager.enable = true;
+  programs = {
+    home-manager.enable = true;
+    git = {
+      enable = true;
+      userName = "Georgian Camarasan";
+      userEmail = "georgian.camarasan@zohomail.eu";
+    };
+    mpv = {
+      enable = true;
+      scripts = with pkgs; [
+        mpvScripts.autoload
+        mpvScripts.uosc
+        mpvScripts.inhibit-gnome
+        mpvScripts.mpv-playlistmanager
+      ];
+    };
+    alacritty = {
+      enable = true;
+    };
+
+    lazygit = {
+      enable = true;
+    };
+  };
 
   home.packages = with pkgs; [
     bitwarden
@@ -27,29 +50,8 @@
     kodi
     nextcloud-client
     pavucontrol
-    lazygit
     calibre
     mc
     mupdf
   ];
-
-  programs.git = {
-    enable = true;
-    userName = "Georgian Camarasan";
-    userEmail = "georgian.camarasan@zohomail.eu";
-  };
-
-  programs.mpv = {
-    enable = true;
-    scripts = with pkgs; [
-      mpvScripts.autoload
-      mpvScripts.uosc
-      mpvScripts.inhibit-gnome
-      mpvScripts.mpv-playlistmanager
-    ];
-  };
-
-  programs.alacritty = {
-    enable = true;
-  };
 }
