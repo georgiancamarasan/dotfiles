040954841a514f16c2617b2451c6043072350b1c
Georgian Camarasan <georgian.camarasan@zohomail.eu>
2023-10-16 20:25:04 +0300

    Improved fish path setting.

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -1,7 +1,16 @@
 if status is-interactive
     # Commands to run in interactive sessions can go here
-    set PATH $PATH:/home/$USER/.cargo/bin
+    if test -d /home/$USER/.cargo/bin
+      set PATH $PATH:/home/$USER/.cargo/bin
+    end
+    if test -d /home/$USER/code/lua-language-server/bin/
+      set PATH $PATH:/home/$USER/code/lua-language-server/bin/
+    end
     fish_vi_key_bindings
 end
-source ~/.config/fish/functions/l.fish
-source ~/.config/fish/functions/ll.fish
+if test -e ~/.config/fish/functions/l.fish
+  source ~/.config/fish/functions/l.fish
+end
+if test -e ~/.config/fish/functions/ll.fish
+  source ~/.config/fish/functions/ll.fish
+end
